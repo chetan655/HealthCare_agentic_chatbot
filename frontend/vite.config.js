14fdfcef4d50a0e6f7f936330f56709ccf25a2c5
chetan <alexantar96@gmail.com>
2026-04-24 18:42:34 +0530

    add fetch to call backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
 
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/chat": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  };
+//   return {
+//     plugins: [react(), tailwindcss()],
+//     server: {
+//       proxy: {
+//         "/chat": {
+//           target: env.VITE_API_URL,
+//           changeOrigin: true,
+//         },
+//       },
+//     },
+//   };
+// });
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+fetch(`${API_URL}/chat`, {
+  method: "POST",
+  body: JSON.stringify(data),
 });
